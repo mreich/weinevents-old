3d4ef3eb3561455f2db240973256f2f44bf09208
Michael Reich <reich.michael@gmail.com>
2012-08-08 23:36:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weinevents::Application.config.secret_token = 'aabf8c494fc825580974169bdc4d1f246805568bbb1761210a6ed4ad444b28e2ef3a6dd9843285707df441b76003df58cf9db6e0aff16b4bc9c281b8b4315bf6'

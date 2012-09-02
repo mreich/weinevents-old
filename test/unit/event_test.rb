adf0631868a065c1f7f9a429b99896b2c2dfe443
Michael Reich <reich.michael@gmail.com>
2012-09-02 11:27:52 -0400

    Updated event_test

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -9,4 +9,30 @@ class EventTest < ActiveSupport::TestCase
     event.end_datetime = '2012-08-22 19:00:00'
     assert event.save
   end
+  
+  test "should find event" do
+    event_id = events(:weinevent_in_erlangen).id
+    assert_nothing_raised { Event.find(event_id) }
+  end
+  
+  test "should update event" do
+    event = events(:weinevent_in_erlangen)
+    assert event.update_attributes(:name => 'New Title')
+  end
+
+  test "should destroy event" do
+    event = events(:weinevent_in_erlangen)
+    event.destroy
+    assert_raise(ActiveRecord::RecordNotFound) {Event.find(event.id)}
+  end
+
+  test "should not create an event without name nor description" do
+    event = Event.new
+    assert !event.valid?
+    assert event.errors[:name].any?
+    assert event.errors[:description].any?
+    assert_equal ["can't be blank"], event.errors[:name]
+    assert_equal ["can't be blank"], event.errors[:description]
+    assert !event.save
+  end
 end

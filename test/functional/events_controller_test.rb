f7a239533d06365ad092b5160c2e263988c6c73c
Michael Reich <reich.michael@gmail.com>
2012-09-02 18:04:30 -0400

    added controller test

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -2,40 +2,48 @@ require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
   setup do
-    @event = events(:one)
+    @event = events(:weinevent_in_erlangen)
   end
 
   test "should get index" do
     get :index
     assert_response :success
+    assert_template 'index'
     assert_not_nil assigns(:events)
   end
 
   test "should get new" do
+    login_as (:michael)
     get :new
     assert_response :success
   end
 
   test "should create event" do
+    login_as (:michael)
     assert_difference('Event.count') do
-      post :create, event: @event.attributes
+      post :create, event: @event => { :name => 'Test Event', :description => 'What a nice test event'}
     end
-
+    assert_response :redirect
     assert_redirected_to event_path(assigns(:event))
   end
 
   test "should show event" do
     get :show, id: @event
     assert_response :success
+    assert_template 'show'
+    assert_not_nil assigns(:event)
+    assert assigns(:event).valid?
   end
 
   test "should get edit" do
-    get :edit, id: @event
+    login_as (:michael)
+    get :edit, id: => @event.to_param
     assert_response :success
   end
 
   test "should update event" do
-    put :update, id: @event, event: @event.attributes
+    login_as (:michael)
+    put :update, id: => @event.to_param, event: @event => {:name => 'Test Event' }
     assert_redirected_to event_path(assigns(:event))
   end
 
